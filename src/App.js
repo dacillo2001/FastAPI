e834d869fa7056b84d1c9a45cc95377e2ef35d3f
dacillo2001 <dacillonicole8@gmail.com>
2025-04-16 16:48:17 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import TaskList from './components/TaskList';
 import Filter from './components/Filter';
 import './App.css';
 
-const API_URL = 'http://127.0.0.1:8000';
-
+const API_URL = 'http://127.0.0.1:8000'; 
 function App() {
   const [tasks, setTasks] = useState([]);
   const [filter, setFilter] = useState('all');
